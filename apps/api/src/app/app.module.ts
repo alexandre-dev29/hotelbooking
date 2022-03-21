858f31e355ece2a8f177272a68a8dc2f9428fd7f
alexandre-dev29 <axel.business29@gmail.com>
2022-03-21 17:39:55 +0200

    Adding the useer ressource

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,12 @@
+import { AuthUserModule } from '@hotelbooking/auth-user';
+import { UtilityModule } from '@hotelbooking/utility';
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [UtilityModule, AuthUserModule],
   controllers: [AppController],
   providers: [AppService],
 })
