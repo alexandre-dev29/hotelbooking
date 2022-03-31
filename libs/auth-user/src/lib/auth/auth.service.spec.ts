b4ad1ad22b41240ac0865f053c83639a0d9f7533
alexandre-dev29 <axel.business29@gmail.com>
2022-03-31 12:35:42 +0200

    updating nx and adding nextjs app

--- libs/auth-user/src/lib/auth/auth.service.spec.ts
+++ libs/auth-user/src/lib/auth/auth.service.spec.ts
@@ -1,9 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
-import {PrismaService, TwilioService, UtilityModule} from "@hotelbooking/utility";
-import {JwtModule} from "@nestjs/jwt";
-import {ConfigModule, ConfigService} from "@nestjs/config";
-import {User} from "@hotelbooking/shared-types";
+import {
+  PrismaService,
+  TwilioService,
+  UtilityModule,
+} from '@hotelbooking/utility';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { User } from '@hotelbooking/shared-types';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -19,7 +23,7 @@ describe('AuthService', () => {
           }),
           inject: [ConfigService],
         }),
-        UtilityModule
+        UtilityModule,
       ],
       providers: [AuthService, PrismaService, TwilioService],
     }).compile();
@@ -33,19 +37,19 @@ describe('AuthService', () => {
 
   it('should register a new user', async function () {
     const username = 'mwenze29';
+    const lastName = 'mwenze29';
     const password = '123456';
     const phoneNumber = '+243814569874';
     const userRole = 'User';
     registereduser = await service.registerUser(
-      {
-        username,
-        password,
-        phoneNumber,
-        userRole,
-      },
+      username,
+      lastName,
+      phoneNumber,
+      password,
+      'testing location'
     );
 
-    expect(registereduser.username).toBe(username);
+    expect(registereduser.firstName).toBe(username);
   });
 
   it('should login user', async function () {
