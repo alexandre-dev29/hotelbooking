266854b2fb6661942be7cce2f4432422c9284327
alexandre-dev29 <axel.business29@gmail.com>
2022-03-31 21:16:09 +0200

    Adding codegen to generate graphql with hooks

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,9 +4,23 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [UtilityModule, AuthUserModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: true,
+    }),
+    UtilityModule,
+    AuthUserModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ libs/shared-types/src/lib/GraphqlGenerated/graphql.tsx
@@ -0,0 +1,71 @@
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type CreateUserInput = {
+  /** Example field (placeholder) */
+  exampleField: Scalars['Int'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createUser: User;
+  removeUser: User;
+  updateUser: User;
+};
+
+export type MutationCreateUserArgs = {
+  createUserInput: CreateUserInput;
+};
+
+export type MutationRemoveUserArgs = {
+  id: Scalars['Int'];
+};
+
+export type MutationUpdateUserArgs = {
+  updateUserInput: UpdateUserInput;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  _service: _Service;
+  user: User;
+};
+
+export type QueryUserArgs = {
+  id: Scalars['Int'];
+};
+
+export type UpdateUserInput = {
+  /** Example field (placeholder) */
+  exampleField?: InputMaybe<Scalars['Int']>;
+  id: Scalars['Int'];
+};
+
+export type User = {
+  __typename?: 'User';
+  /** Example field (placeholder) */
+  exampleField: Scalars['Int'];
+};
+
+export type _Service = {
+  __typename?: '_Service';
+  /** The sdl representing the federated service capabilities. Includes federation directives, removes federation types, and includes rest of full schema after schema directives have been applied */
+  sdl?: Maybe<Scalars['String']>;
+};
