95be734e3a82938b4427800eebf3cbaf9fcc4f21
alexandre-dev29 <axel.business29@gmail.com>
2022-04-18 13:27:21 +0200

    Edit the user context code

--- libs/shared-ui-component/src/lib/UserContextUi.ts
+++ libs/shared-ui-component/src/lib/UserContextUi.ts
@@ -3,6 +3,6 @@ import { createContext, useContext } from 'react';
 export const UserContext = createContext(null);
 
 export const useUser = () => {
-  const user = useContext(UserContext);
-  return { user };
+  const currentUser = useContext(UserContext);
+  return { currentUser };
 };
