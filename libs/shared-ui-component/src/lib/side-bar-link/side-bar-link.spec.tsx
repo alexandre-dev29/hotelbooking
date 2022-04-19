a73f1398cf1402e8b229ea58f13dcdbf3c8bd711
alexandre-dev29 <axel.business29@gmail.com>
2022-04-19 09:38:55 +0200

    Updating tests

--- libs/shared-ui-component/src/lib/side-bar-link/side-bar-link.spec.tsx
+++ libs/shared-ui-component/src/lib/side-bar-link/side-bar-link.spec.tsx
@@ -1,10 +1,12 @@
 import { render } from '@testing-library/react';
-
 import SideBarLink from './side-bar-link';
+import { FaArrowRight } from 'react-icons/fa';
 
 describe('SideBarLink', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<SideBarLink />);
+    const { baseElement } = render(
+      <SideBarLink iconElement={<FaArrowRight />} linkTitle={'Testing'} />
+    );
     expect(baseElement).toBeTruthy();
   });
 });
